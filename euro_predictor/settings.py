16d67bf522b0cef164db9e2c9559c89c152affe2
Rich Longdon <rglongdon@gmail.com>
2024-06-10 21:25:44 +0100

    static added

--- euro_predictor/settings.py
+++ euro_predictor/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-18)=5b@wn!xxa+dpsm-jyr%))okc=aa5f9=_-^ankbyean$r)c
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
